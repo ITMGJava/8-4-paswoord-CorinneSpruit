d21eb829a7cbca9f74c50094cbe0f41db04981eb
CorinneSpruit <corinnemeijdam@hotmail.com>
2020-10-20 19:46:08 +0200

    hoi h

--- src/Paswoord.java
+++ src/Paswoord.java
@@ -1,81 +1,61 @@
+import com.sun.source.tree.WhileLoopTree;
+
+import java.nio.charset.CoderResult;
 import java.util.Scanner;
 
 public class Paswoord {
     public static void main(String[] args) {
 
-        boolean check = true;
-        do {
+
+        var user1 = "Corinne";
+        var user2 = "Iris";
+        var user3 = "Lisa";
+        var user4 = "Rosie";
+
+        var x = new Scanner(System.in);
+
+
+        boolean CorrecteInvoer = true;
+
 
             System.out.println("Voer je gebruikersnaam in");
-
-            var x = new Scanner(System.in);
             var naam = x.nextLine();
 
-            var user1 = "Corinne";
-            var user2 = "Iris";
-            var user3 = "Lisa";
-            var user4 = "Rosie";
 
-            if (naam.equals(user1)) {
-                check = true;
-                System.out.println("Voer je wachtwoord in");
-
-            } else if (naam.equals(user2)) {
-                check = true;
-                System.out.println("Voer je wachtwoord in");
-
-            } else if (naam.equals(user3)) {
-                check = true;
-                System.out.println("Voer je wachtwoord in");
-
-
-            } else if (naam.equals(user4)) {
-                check = true;
-                System.out.println("Voer je wachtwoord in");
-
-            } else if (naam.equals(default)) {
-                check=false;
-                System.out.println("Verkeerde inlognaam");
-
+            if (naam.equals(user1) | naam.equals(user2) | naam.equals(user3) | naam.equals(user4)) {
+                CorrecteInvoer = true;
+                System.out.println("Gebruikersnaam correct");
+            } else {
+                System.out.println("Gebruikersnaam onjuist, probeer nogmaals");
             }
 
-                do {
+            System.out.println("Voer je wachtwoord in");
 
-                    var y = new Scanner(System.in);
-                    var pw = y.nextLine();
+            var y = new Scanner(System.in);
+            var pw = y.nextLine();
 
+            var pw1 = "123";
+            var pw2 = "234";
+            var pw3 = "345";
+            var pw4 = "456";
 
-                    var pw1 = "123";
-                    var pw2 = "123";
-                    var pw3 = "123";
-                    var pw4 = "123";
-
-                    if (naam.equals(user1) & pw.equals(pw1)) {
-                        check = true;
-                        System.out.println("Je bent ingelogd");
-
-                    } else if (naam.equals(user2) & pw.equals(pw1)) {
-                        check = true;
-                        System.out.println("je bent ingelogd");
-
-                    } else if (naam.equals(user1) & pw.equals(pw3)) {
-                        check = true;
-                        System.out.println("je bent ingelogd");
+            if (naam.equals(user1) & pw.equals(pw1) | naam.equals(user2) & pw.equals(pw2) |
+                    naam.equals(user3) & pw.equals(pw3) | naam.equals(user4) & pw.equals(pw4)) {
+                CorrecteInvoer = true;
+                System.out.println("Je bent ingelogd");
+            } else
+            {
+                System.out.println("Paswoord onjuist");
+            }
 
 
-                    } else if (naam.equals(user1) & pw.equals(pw4)) {
-                        check = true;
-                        System.out.println("je bent ingelog");
-
-                    } else if (naam.equals(false) & pw.equals(default)) {
-                        System.out.println("Verkeerd wachtwoord");
-
-                    }
-                } while (!check);
-//
-
-        }while (check);
+        }
+        }
 
 
-    }
-}
+
+
+
+
+
+
